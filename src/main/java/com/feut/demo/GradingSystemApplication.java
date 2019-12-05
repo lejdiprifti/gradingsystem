57e942bf385657c843bec9808a195743fcde18d7
lejdiprifti <priftilejdi@gmail.com>
2019-12-05 02:06:03 +0100

    git init

---
+++ src/main/java/com/feut/demo/GradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.feut.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feut/demo/GradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feut.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
