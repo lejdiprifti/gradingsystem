67d7f6a43de823cbb7b480b089cbad26b30f9627
lejdiprifti <priftilejdi@gmail.com>
2020-01-03 19:31:04 +0100

    deployed app in firebase

--- server-app/src/main/java/com/feut/resource/LoginResource.java
+++ server-app/src/main/java/com/feut/resource/LoginResource.java
@@ -17,7 +17,7 @@ import com.feut.service.LoginService;
 
 @RestController
 @RequestMapping(path="/login", consumes="application/json", produces="application/json")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://myfeut.firebaseapp.com")
 public class LoginResource {
 	
 	private static Logger logger = LogManager.getLogger(LoginResource.class);
